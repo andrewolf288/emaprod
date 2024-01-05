58f977c192a5ca8ed9275730b1f75c62f32803a5
John Dow <ajacoboc@unsa.edu.pe>
2024-01-05 02:36:00 -0500

    FORMATOS DE IMPORTACION DE ATRIBUTOS CALIDAD FRESCOS COMPLETO

--- Backend/scripts/atributos-calidad/createAtributosCalidadCulantro.php
+++ Backend/scripts/atributos-calidad/createAtributosCalidadCulantro.php
@@ -11,7 +11,7 @@ $description_error = "";
 // este script sirve para resetear las entradas existentes
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $idProdt = 130; // tomate
+    $idProdt = 131; // CULANTRO FRESCO
     // Preparar la consulta de inserción una sola vez fuera del bucle
     $sql_create_atributos_calidad_materias_primas = "INSERT INTO 
     producto_atributo_calidad (idProdt, nomProdAtr, idTipProdAtr, opcProdAtr, codGruAtr, labGruAtr)
@@ -21,14 +21,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Ejecutar la consulta preparada para la inserción con los valores específicos
     $atributos_calidad = [
-        ['CALIBRE', 6, 'Calibre GG (82 - 102 mm),Calibre GG (67 - 82 mm),Calibre GG (57 - 67 mm),Calibre GG (47 - 57 mm)', 1, "CALIBRE"],
-        ['% MADUROS', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['% PINTONES', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['% VERDES', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['% CICATRISADOS', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['% SOBREMADUROS, PICADOS, APLASTADOS', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['pH', 1, '', 3, 'ANALISIS FISICOQUIMICOS'],
-        ['ST (°Brix)', 1, '', 3, 'ANALISIS FISICOQUIMICOS'],
+        ['% FRESCA', 1, '', 1, 'ANALISIS POSTCOSECHA'],
+        ['% MACHITEZ', 1, '', 1, 'ANALISIS POSTCOSECHA'],
+        ['% CON FLORES', 1, '', 1, 'ANALISIS POSTCOSECHA'],
+        ['% DE IMPUREZAS', 1, '', NULL, NULL],
+        ['GROSOR DE TALLO', 6, '1er corte (1-2 mm),2do corte (2-3 mm),3er corte (>5 mm)', 2, "GROSOR DE TALLO"],
+        ['TAMAÑO (MAX. 25 CM)', 4, 'C,LI,I', NULL, NULL],
+        ['% TALLOS', 1, '', 3, 'COMPOSICION'],
+        ['% HOJAS', 1, '', 3, 'COMPOSICION'],
+        ['% HUMEDAD', 1, '', 4, 'ANALISIS FISICOQUIMICOS'],
+        ['ST (°Brix)', 1, '', 4, 'ANALISIS FISICOQUIMICOS'],
         ['V°B PROD.', 4, 'Vilma,Carlos', NULL, NULL]
     ];
 
--- Backend/scripts/atributos-calidad/createAtributosCalidadEspinaca.php
+++ Backend/scripts/atributos-calidad/createAtributosCalidadEspinaca.php
@@ -11,7 +11,7 @@ $description_error = "";
 // este script sirve para resetear las entradas existentes
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $idProdt = 130; // tomate
+    $idProdt = 132; // espinaca
     // Preparar la consulta de inserción una sola vez fuera del bucle
     $sql_create_atributos_calidad_materias_primas = "INSERT INTO 
     producto_atributo_calidad (idProdt, nomProdAtr, idTipProdAtr, opcProdAtr, codGruAtr, labGruAtr)
@@ -21,14 +21,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Ejecutar la consulta preparada para la inserción con los valores específicos
     $atributos_calidad = [
-        ['CALIBRE', 6, 'Calibre GG (82 - 102 mm),Calibre GG (67 - 82 mm),Calibre GG (57 - 67 mm),Calibre GG (47 - 57 mm)', 1, "CALIBRE"],
-        ['% MADUROS', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['% PINTONES', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['% VERDES', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['% CICATRISADOS', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['% SOBREMADUROS, PICADOS, APLASTADOS', 1, '', 2, 'ANALISIS POSTCOSECHA'],
-        ['pH', 1, '', 3, 'ANALISIS FISICOQUIMICOS'],
-        ['ST (°Brix)', 1, '', 3, 'ANALISIS FISICOQUIMICOS'],
+        ['% FRESCA', 1, '', 1, 'ANALISIS POSTCOSECHA'],
+        ['% MACHITEZ', 1, '', 1, 'ANALISIS POSTCOSECHA'],
+        ['% HOJAS AMARILLAS', 1, '', 1, 'ANALISIS POSTCOSECHA'],
+        ['% SEMILLAS', 1, '', 1, 'ANALISIS POSTCOSECHA'],
+        ['% PUTRICIONES', 1, '', 1, 'ANALISIS POSTCOSECHA'],
+        ['TAMAÑO PROMEDIO', 1, '', 2, 'ANALISIS FISICOQUIMICOS'],
+        ['ST (°Brix)', 1, '', 2, 'ANALISIS FISICOQUIMICOS'],
         ['V°B PROD.', 4, 'Vilma,Carlos', NULL, NULL]
     ];
 
