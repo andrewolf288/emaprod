6ca1bbf39a3fece90302ca8b1f88e9f84f0a1943
andrewolf288 <pold.jacobo89@gmail.com>
2024-02-05 08:52:45 -0500

    Reporte de productos finales por lote al 100%

--- Backend/almacen/reportes/reporte-producto-final-lote.php
+++ Backend/almacen/reportes/reporte-producto-final-lote.php
@@ -22,14 +22,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $json = file_get_contents('php://input');
     $data = json_decode($json, true);
     $almacen = $data["almacen"];
+    $idEntStoEst = 1;
 
-    $result["header"] = ["Almacen", "EMAPROD", "Producto", "Saldo producto", "Medida", "Lote", "Saldo lote"];
-    $result["columnWidths"] = [25, 13, 90, 13, 8, 11, 13];
-    $result["tipoDato"] = ["texto", "texto", "texto", "numero", "texto", "texto", "numero"];
+    $result["header"] = ["Almacen", "SIIGO", "EMAPROD", "Producto", "Saldo producto", "Medida", "Lote", "Saldo lote", "Fecha vencimiento"];
+    $result["columnWidths"] = [25, 13, 13, 67, 14, 8, 10, 14, 18];
+    $result["tipoDato"] = ["texto", "texto", "texto", "texto", "numero", "texto", "texto", "numero", "texto"];
 
     $claseProductoFinal = 2;
     $sql_select_productos_finales =
-        "SELECT p.id, p.nomProd, p.codProd2, me.simMed, p.idCla
+        "SELECT p.id, p.nomProd, p.codProd2, p.codProd, me.simMed, p.idCla
         FROM producto AS p 
         JOIN medida AS me ON me.id = p.idMed
         WHERE p.idCla = ?";
@@ -41,6 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $idProd = $row_producto_final["id"]; // id producto
         $nomProd = $row_producto_final["nomProd"]; // nombre del producto
         $codProd2 = $row_producto_final["codProd2"]; // codigo EMPAROD
+        $codProd = $row_producto_final["codProd"]; // codigo SIIGO
         $simMed = $row_producto_final["simMed"]; // medida
         $nomAlm = "";
         $canSto = 0.0;
@@ -63,15 +65,79 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             $auxStockTotal = array(
                 "nomAlm" => $nomAlm,
+                "codProd" => $codProd,
                 "codProd2" => $codProd2,
                 "nomProd" => $nomProd,
                 "canSto" => $canSto,
                 "simMed" => $simMed,
                 "codLotProd" => "",
-                "salLotProd" => ""
+                "salLotProd" => "",
+                "fecVenEntSto" => ""
             );
 
             array_push($result["data"], $auxStockTotal);
+        }
+
+        //ahora recorremos las entradas en busca de los lotes
+        $select_entradas_producto_final =
+            "SELECT es.codLot, es.canTotDis, DATE(es.fecVenEntSto) AS fecVenEntSto, es.refProdc 
+        FROM entrada_stock AS es
+        WHERE es.idAlm = ? AND es.idProd = ? AND es.idEntStoEst = ? AND es.canTotDis > 0";
+        $stmt_select_entradas_producto_final = $pdo->prepare($select_entradas_producto_final);
+        $stmt_select_entradas_producto_final->bindParam(1, $almacen, PDO::PARAM_INT);
+        $stmt_select_entradas_producto_final->bindParam(2, $idProd, PDO::PARAM_INT);
+        $stmt_select_entradas_producto_final->bindParam(3, $idEntStoEst, PDO::PARAM_INT);
+        $stmt_select_entradas_producto_final->execute();
+
+        $rows_entradas_producto_final = $stmt_select_entradas_producto_final->fetchAll(PDO::FETCH_ASSOC);
+
+        // Array asociativo para almacenar las sumas por refProdc
+        $sumas_por_refProdc = array();
+
+        // Recorrer los registros y realizar la suma
+        foreach ($rows_entradas_producto_final as $registro) {
+            $refProdc = $registro['refProdc'];
+            $canTotDis = $registro['canTotDis'];
+
+            // Utilizar el operador de fusión de null (??) para manejar el caso cuando no existe la clave
+            $sumas_por_refProdc[$refProdc] = ($sumas_por_refProdc[$refProdc] ?? 0) + $canTotDis;
+        }
+
+        // Construir un nuevo array con un solo registro por refProdc y la cantidad total sumada
+        $nuevos_registros = array();
+
+        foreach ($rows_entradas_producto_final as $registro) {
+            $refProdc = $registro['refProdc'];
+
+            // Utilizar el operador de fusión de null (??) para manejar el caso cuando no existe la clave
+            $registro['canTotDis'] = $sumas_por_refProdc[$refProdc] ?? 0;
+
+            // Agregar el registro al nuevo array solo si aún no ha sido agregado
+            if (!isset($nuevos_registros[$refProdc])) {
+                $nuevos_registros[$refProdc] = $registro;
+            }
+        }
+
+        // Ahora $nuevos_registros contiene un solo registro por refProdc con la cantidad total sumada
+        $nuevos_registros = array_values($nuevos_registros); // Reindexar el array si es necesario
+        // print_r($nuevos_registros);
+
+        foreach ($nuevos_registros as $registro) {
+            $canLoteStockTotal = $registro["canTotDis"];
+            $codLot = $registro["codLot"];
+            $fecVenEntSto = $registro["fecVenEntSto"];
+            $auxLoteStockTotal = array(
+                "nomAlm" => "",
+                "codProd" => "",
+                "codProd2" => "",
+                "nomProd" => "",
+                "canSto" => "",
+                "simMed" => $simMed,
+                "codLotProd" => $codLot,
+                "salLotProd" => $canLoteStockTotal,
+                "fecVenEntSto" => $fecVenEntSto
+            );
+            array_push($result["data"], $auxLoteStockTotal);
         }
     }
 
@@ -115,7 +181,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         foreach ($columnNames as $columnIndex => $value) {
             $columnLetter = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($columnIndex + 1);
             $sheet->setCellValue("{$columnLetter}{$row}", $rowData[$value]);
+            if ($value == "fecVenEntSto") {
+                $sheet->getStyle("{$columnLetter}{$row}")->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+            }
         }
+
+        // Obtener el valor en el key "motOpe"
+        $nomProd = $rowData["nomProd"];
+        if (empty($nomProd)) {
+            $sheet->getStyle("A{$row}:I{$row}")->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('93d98d'); // Verde
+        } else {
+            $sheet->getStyle("A{$row}:I{$row}")->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setRGB('FFFF00'); // amarillo
+        }
+
         $row++;
     }
 
